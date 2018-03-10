9cfa4e5d56cc8006d76fc93c7a689a0177cb2f17
Lisa Lee <lisalee@Lisas-MacBook-Pro.local>
2018-03-10 13:46:05 -0600

    half of user controller

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,3 +1,40 @@
 class UserController < ApplicationController
+    def login 
+        @user = User.find_by(user_params)
+        if @user 
+            render(status: 201, json: @user)
+        else 
+            render(status: 404)
+        end 
+    end 
+
+    def signup
+        render(status: 201, json: User.create(user_params))
+    end 
+
+    def logout 
+
+    end 
+
+    def show 
+
+    end
+
+    private 
+
+    def user_params 
+        params.require(:user).permit(:email, :password)
+    end 
 
 end
+
+
+# router.delete('/logout', (req, res, next) => {
+#   req.logout();
+#   res.sendStatus(204);
+# });
+
+# router.get('/', (req, res, next) => {
+#   res.send(req.user);
+# })
+# module.exports = router;
